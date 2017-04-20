91643a690a27b26efa3838c48dc5f2894bf25eeb
Michael Sargent <michaelksarge@gmail.com>
2017-04-20 13:14:20 -0700

    Reorganize files

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ export { createBrowserHistory } from './history/createBrowserHistory';
 export { startListener } from './history/startListener';
 
 // Redux API
-export { PUSH, REPLACE, GO, GO_BACK, GO_FORWARD, MANUAL_CHANGE } from './redux/constants';
-export { push, replace, go, goBack, goForward, manualChange } from './redux/actions';
-export { routerReducer } from './redux/reducer';
 export { routerMiddleware } from './redux/middleware';
+export { routerReducer } from './redux/reducer';
+export { push, replace, go, goBack, goForward, manualChange } from './redux/actions';
+export { PUSH, REPLACE, GO, GO_BACK, GO_FORWARD, MANUAL_CHANGE } from './redux/constants';
 
 // React API
 export { RouterContainer as Router } from './react/Router';
