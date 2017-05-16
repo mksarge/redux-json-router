767929d0fb9add78ab9ec486e6e95e6789360cad
Michael Sargent <michaelksarge@gmail.com>
2017-05-16 14:06:08 -0700

    Update dependencies and test setup

--- test/.setup.js
+++ test/.setup.js
@@ -1,23 +1,22 @@
-// enzyme + mocha setup
-// https://github.com/lelandrichardson/enzyme-example-mocha/blob/master/test/.setup.js
+// Enzyme/Mocha setup: https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
 
 require('babel-register')();
+const { JSDOM } = require('jsdom');
 
-var jsdom = require('jsdom').jsdom;
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
 
-var exposedProperties = ['window', 'navigator', 'document'];
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter((prop) => typeof target[prop] === 'undefined')
+    .map((prop) => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
 
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
-
+global.window = window;
+global.document = window.document;
 global.navigator = {
-  userAgent: 'node.js'
+  userAgent: 'node.js',
 };
 
-documentRef = document;
+copyProps(window, global);
