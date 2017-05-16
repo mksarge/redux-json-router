ab3f0d14c998384b161c966b114c4793e1cb2795
Michael Sargent <michaelksarge@gmail.com>
2017-05-16 14:34:46 -0700

    Fix location dispatched in history listener

--- src/history/startListener.js
+++ src/history/startListener.js
@@ -5,11 +5,11 @@ import { manualChange } from '../redux/actions';
  * and whenever a popstate navigation event occurs.
  */
 function startListener(history, store) {
-  store.dispatch(manualChange(history.location.path));
+  store.dispatch(manualChange(`${history.location.pathname}${history.location.search}${history.location.hash}`));
 
   history.listen((location, action) => {
     if (action === 'POP') {
-      store.dispatch(manualChange(location.path));
+      store.dispatch(manualChange(`${location.pathname}${location.search}${location.hash}`));
     }
   });
 }
